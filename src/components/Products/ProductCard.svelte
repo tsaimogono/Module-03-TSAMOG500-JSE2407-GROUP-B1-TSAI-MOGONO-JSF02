59af1a8717403ca33f0337e4fdae623395ea37ce
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-07-31 01:17:28 +0200

    Exported properties id, title, image, price, rating, and category to be passed from parent component

---
+++ src/components/Products/ProductCard.svelte
@@ -0,0 +1,12 @@
+<script>
+    //import { onMount } from 'svelte';
+    import Ratings from './Ratings.svelte';
+    import { navigate } from 'svelte-routing';
+  
+    //Exporting properties that can be passed from a parent component
+    export let id;
+    export let title;
+    export let image;
+    export let price;
+    export let rating;
+    export let category;
