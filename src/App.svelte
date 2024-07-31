535846bcea24b61ea4d6b3203232e94a394b73b2
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-07-31 02:48:12 +0200

    Set up routing with svelte-routing and add Navbar component - Implemented Router and Route for Products and ProductDetail components - Included Navbar component for consistent navigation

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,21 @@
 <script>
-  import Header from './components/Header.svelte'
-  import ProductDetails from './components/Products/ProductDetails.svelte'
-  import {Router, Route} from "svelte-routing"
-  import ProductDetail from './components/Products/ProductDetail.svelte'
-  
+  import { Router, Route} from 'svelte-routing';
+  import Products from './Routes/+Products.svelte';
+  import ProductDetail from '../src/Routes/+ProductDetail.svelte';
+  import Navbar from './Routes/+Navbar.svelte';
 </script>
 
-<style lang="postcss">
-  :global(html) {
-      background-color: #caf0f8;
-  }
+<Navbar />
 
+<main>
+    <Router>
+        <Route path="/" component={Products} />
+        <Route path="/product/:id" component={ProductDetail} />
+    </Router>
+</main>
+
+<style>
+    main {
+        padding: 20px;
+    }
 </style>
-
-<Router>
-      <Header />
-  
-  <Route path="/" component={ProductDetails} />
-  <Route path="/products/:id" component={ ProductDetail}/>
-</Router>
